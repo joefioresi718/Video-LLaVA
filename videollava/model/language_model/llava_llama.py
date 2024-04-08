8cd771cda5e38c26dbd025b9c653b590f05b198c
joefioresi718 <joefioresi718@gmail.com>
2024-04-08 13:48:08 -0400

    comment out generate overwrite

--- videollava/model/language_model/llava_llama.py
+++ videollava/model/language_model/llava_llama.py
@@ -51,44 +51,44 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
         # Initialize weights and apply final processing
         self.post_init()
 
-    @torch.no_grad()
-    def generate(
-        self,
-        inputs: Optional[torch.Tensor] = None,
-        images: Optional[torch.Tensor] = None,
-        **kwargs,
-    ) -> Union[GenerateOutput, torch.LongTensor]:
-        position_ids = kwargs.pop("position_ids", None)
-        attention_mask = kwargs.pop("attention_mask", None)
+    # @torch.no_grad()
+    # def generate(
+    #     self,
+    #     inputs: Optional[torch.Tensor] = None,
+    #     images: Optional[torch.Tensor] = None,
+    #     **kwargs,
+    # ) -> Union[GenerateOutput, torch.LongTensor]:
+    #     position_ids = kwargs.pop("position_ids", None)
+    #     attention_mask = kwargs.pop("attention_mask", None)
 
-        if "inputs_embeds" in kwargs:
-            raise NotImplementedError("`inputs_embeds` is not supported")
+    #     if "inputs_embeds" in kwargs:
+    #         raise NotImplementedError("`inputs_embeds` is not supported")
         
-        if images is not None:
-            (
-                inputs,
-                position_ids,
-                attention_mask,
-                _,
-                inputs_embeds,
-                _
-            ) = self.prepare_inputs_labels_for_multimodal(
-                inputs,
-                position_ids,
-                attention_mask,
-                None,
-                None,
-                images,
-            )
-        else:
-            inputs_embeds = self.get_model().embed_tokens(inputs)
+    #     if images is not None:
+    #         (
+    #             inputs,
+    #             position_ids,
+    #             attention_mask,
+    #             _,
+    #             inputs_embeds,
+    #             _
+    #         ) = self.prepare_inputs_labels_for_multimodal(
+    #             inputs,
+    #             position_ids,
+    #             attention_mask,
+    #             None,
+    #             None,
+    #             images,
+    #         )
+    #     else:
+    #         inputs_embeds = self.get_model().embed_tokens(inputs)
         
-        return super().generate(
-            position_ids=position_ids,
-            attention_mask=attention_mask,
-            inputs_embeds=inputs_embeds,
-            **kwargs
-        )
+    #     return super().generate(
+    #         position_ids=position_ids,
+    #         attention_mask=attention_mask,
+    #         inputs_embeds=inputs_embeds,
+    #         **kwargs
+    #     )
 
     def get_model(self):
         return self.model
