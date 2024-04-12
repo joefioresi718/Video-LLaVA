51b6d279d75ff57f90817c3e33370a6f029d6157
joefioresi718 <joefioresi718@gmail.com>
2024-04-12 14:56:46 -0400

    update eval scripts

--- scripts/v1_5/eval/eval_image_pope.sh
+++ scripts/v1_5/eval/eval_image_pope.sh
@@ -3,11 +3,13 @@
 
 CKPT_NAME="Video-LLaVA-7B"
 CKPT="checkpoints/${CKPT_NAME}"
+CKPT="LanguageBind/Video-LLaVA-7B"
 EVAL="eval"
+EVAL_COCO="/datasets/MSCOCO"
 python3 -m videollava.eval.model_vqa_loader \
     --model-path ${CKPT} \
     --question-file ${EVAL}/pope/llava_pope_test.jsonl \
-    --image-folder ${EVAL}/pope/val2014 \
+    --image-folder ${EVAL_COCO}/val2014 \
     --answers-file ${EVAL}/pope/answers/${CKPT_NAME}.jsonl \
     --temperature 0 \
     --conv-mode vicuna_v1
