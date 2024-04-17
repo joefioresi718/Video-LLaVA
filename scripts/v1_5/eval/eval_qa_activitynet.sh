af43296309983b4a62f0ebbd9c0b958e4f8a8043
joefioresi718 <joefioresi718@gmail.com>
2024-04-17 12:21:47 -0400

    update eval bashes

--- scripts/v1_5/eval/eval_qa_activitynet.sh
+++ scripts/v1_5/eval/eval_qa_activitynet.sh
@@ -6,7 +6,7 @@ pred_path="${GPT_Zero_Shot_QA}/Activitynet_Zero_Shot_QA/${output_name}/merge.jso
 output_dir="${GPT_Zero_Shot_QA}/Activitynet_Zero_Shot_QA/${output_name}/mixtral_8x7B"
 output_json="${GPT_Zero_Shot_QA}/Activitynet_Zero_Shot_QA/${output_name}/results.json"
 model_name="mistralai/Mixtral-8x7B-Instruct-v0.1"
-num_tasks=8
+# model_name="mistralai/Mistral-7B-Instruct-v0.2"
 
 
 
@@ -14,5 +14,4 @@ python3 videollava/eval/video/mistral_eval_video_qa.py \
     --pred_path ${pred_path} \
     --output_dir ${output_dir} \
     --output_json ${output_json} \
-    --model_name ${model_name} \
-    --num_tasks ${num_tasks}
+    --model_name ${model_name}
--- scripts/v1_5/eval/eval_qa_msrvtt.sh
+++ scripts/v1_5/eval/eval_qa_msrvtt.sh
@@ -3,18 +3,14 @@
 GPT_Zero_Shot_QA="eval/GPT_Zero_Shot_QA"
 output_name="Video-LLaVA-7B"
 pred_path="${GPT_Zero_Shot_QA}/MSRVTT_Zero_Shot_QA/${output_name}/merge.jsonl"
-output_dir="${GPT_Zero_Shot_QA}/MSRVTT_Zero_Shot_QA/${output_name}/gpt"
+output_dir="${GPT_Zero_Shot_QA}/MSRVTT_Zero_Shot_QA/${output_name}/mixtral_8x7B"
 output_json="${GPT_Zero_Shot_QA}/MSRVTT_Zero_Shot_QA/${output_name}/results.json"
-api_key=""
-api_base=""
-num_tasks=8
+model_name="mistralai/Mixtral-8x7B-Instruct-v0.1"
+# model_name="mistralai/Mistral-7B-Instruct-v0.2"
 
 
-
-python3 videollava/eval/video/eval_video_qa.py \
+python3 videollava/eval/video/mistral_eval_video_qa.py \
     --pred_path ${pred_path} \
     --output_dir ${output_dir} \
     --output_json ${output_json} \
-    --api_key ${api_key} \
-    --api_base ${api_base} \
-    --num_tasks ${num_tasks}
+    --model_name ${model_name}
